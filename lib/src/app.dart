51e2d3432271799493ab7957cfc4d98f534ea481
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2024-03-15 19:36:54 +0600

    lab1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,6 +7,7 @@ class CoffeeShopApp extends StatelessWidget {
   const CoffeeShopApp({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
